088d1da4eb0a39a284bffd19f1b6522b084025d6
Josh Crawford <joshua@sgroup.com.au>
2019-02-19 21:18:31 +1100

    Add logging

--- src/services/Service.php
+++ src/services/Service.php
@@ -26,18 +26,30 @@ class Service extends Component
         $currentUser = $event->identity;
         $defaultUser = Craft::$app->users->getUserById($settings->userDashboard);
 
+        if (!$defaultUser) {
+            DefaultDashboard::error("Default User not found for ID: $settings->userDashboard");
+            return;
+        }
+
         // Proceed if we've got a setting for the default user, and its not that user logging in
-        if (!$defaultUser || $currentUser->id == $defaultUser->id) {
+        if ($currentUser->id == $defaultUser->id) {
+            DefaultDashboard::log("Skip setting dashboard - this is the default user");
             return;
         }
 
         $currentUserWidgets = $this->_getUserWidgets($currentUser->id);
         $defaultUserWidgets = $this->_getUserWidgets($defaultUser->id);
 
+        DefaultDashboard::log("Current User ID: " . $currentUser->id);
+        DefaultDashboard::log("Default User ID: " . $defaultUser->id);
+        DefaultDashboard::log("Current User Widget: " . json_encode($this->_widgets($currentUserWidgets)));
+        DefaultDashboard::log("Default User Widget: " . json_encode($this->_widgets($defaultUserWidgets)));
+
         // If this user has no widgets, create them and finish - or, if we're forcing override
         if (!$currentUserWidgets || $settings->override) {
             // To prevent massive re-creating of widgets each login, check if default vs current is different
             if ($this->_compareWidgets($currentUserWidgets, $defaultUserWidgets)) {
+                DefaultDashboard::log("Users widgets are the same");
                 return;
             }
 
@@ -77,6 +89,9 @@ class Service extends Component
         $areSame = true;
 
         if (count($currentUserWidgets) != count($defaultUserWidgets)) {
+            DefaultDashboard::log("Current User Widgets Count: " . count($currentUserWidgets));
+            DefaultDashboard::log("Default User Widgets Count: " . count($defaultUserWidgets));
+
             return false;
         }
 
@@ -98,6 +113,9 @@ class Service extends Component
                 'colspan' => $defaultUserWidget['colspan'],
                 'settings' => $defaultUserWidget['settings'],
             ];
+
+            DefaultDashboard::log("Current Widgets: " . json_encode($array1));
+            DefaultDashboard::log("Default Widgets: " . json_encode($array2));
 
             if (array_diff($array1, $array2)) {
                 $areSame = false;
@@ -130,5 +148,16 @@ class Service extends Component
 
             throw $e;
         }
+    }
+
+    private function _widgets($widgets)
+    {
+        $array = [];
+
+        foreach ($widgets as $widget) {
+            $array[] = $widget->toArray();
+        }
+
+        return $array;
     }
 }
