dd6b50649ad2be4157eeb57827922faf5d631706
Josh Crawford <joshua@sgroup.com.au>
2022-07-12 16:09:14 +1000

    Translations

---
+++ src/translations/en/default-dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'Default Dashboard' => 'Default Dashboard',
+	'Settings' => 'Settings',
+	'Default User Dashboard' => 'Default User Dashboard',
+	'Select a user to use as the default dashboard, mirrored to all other users.' => 'Select a user to use as the default dashboard, mirrored to all other users.',
+	'Override User Dashboard' => 'Override User Dashboard',
+	'Whether to force the default dashboard, overwriting any user-specific ones. This will occur on each login.' => 'Whether to force the default dashboard, overwriting any user-specific ones. This will occur on each login.',
+	'Exclude Admin User' => 'Exclude Admin User',
+	'Whether to exclude the admin user when force overwrite is enabled. This will occur on each login.' => 'Whether to exclude the admin user when force overwrite is enabled. This will occur on each login.',
+];
