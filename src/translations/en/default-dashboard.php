635fd8a32c7f267c07d24acbf0379b86d8cab75a
Josh Crawford <joshua@sgroup.com.au>
2024-05-27 14:52:06 +1000

    Update English translations.

--- src/translations/en/default-dashboard.php
+++ src/translations/en/default-dashboard.php
@@ -1,4 +1,10 @@
 <?php
 
 return [
+  'Default Dashboard' => 'Default Dashboard',
+  'Default User Dashboard' => 'Default User Dashboard',
+  'Exclude Admin User' => 'Exclude Admin User',
+  'Override User Dashboard' => 'Override User Dashboard',
+  'Whether to exclude the admin user when force overwrite is enabled. This will occur on each login.' => 'Whether to exclude the admin user when force overwrite is enabled. This will occur on each login.',
+  'Whether to force the default dashboard, overwriting any user-specific ones. This will occur on each login.' => 'Whether to force the default dashboard, overwriting any user-specific ones. This will occur on each login.',
 ];
